10f955c27ef140d8832cc1a713eb374df4c0dfbb
apuni866 <112840100+apuni866@users.noreply.github.com>
2024-09-25 13:23:25 -0600

    Add files via upload

---
+++ proj1work/custom_string.h
@@ -0,0 +1,20 @@
+#ifndef CUSTOM_STRING_H
+#define CUSTOM_STRING_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+
+
+int string_compare(const char * string1, const char* string2, size_t byte_length);
+int parse (char *input_buffer, char *args[]);
+char *strcpy (char *destination, const char *source); 
+
+size_t get_strlen(const char *input_str);
+
+char *strncat(char *destination, const char *source, size_t src_byte_len);
+
+// char *strcat(char *destination, const char *source);  
+
+#endif
