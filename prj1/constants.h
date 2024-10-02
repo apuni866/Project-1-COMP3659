8dad5b007e155eb63d1e69010553e5f10c42456a
evan wushke <ewush956@mtroyal.ca>
2024-10-02 20:56:52 +0000

    Added redirect constants

--- prj1/constants.h
+++ prj1/constants.h
@@ -1,7 +1,7 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define MAX_BUFFER_SIZE 256     //256
+#define MAX_BUFFER_SIZE 256 
 #define MAX_LEN 128
 #define MAX_ARGS 64
 #define STD_INPUT_FD 0
@@ -9,6 +9,9 @@
 #define PROMPT_SYMBOL_SIZE 4
 #define EXIT_SUCCESS 0
 #define EXIT_FAIL 1
+#define NULL '\0'
+#define OUT_REDIRECT_CODE 1
+#define IN_REDIRECT_CODE 2
 
 typedef enum {
   false = 0,
