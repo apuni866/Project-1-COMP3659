0726a87b59d68691c18cc0f516e4d53e7ae431ae
Amtoj Punia <apuni866@mtroyal.ca>
2024-10-18 17:26:15 +0000

    added signal stuff with evans cleaned yp vers'n

---
+++ prj1/custom_signals.c
@@ -0,0 +1,40 @@
+#include "custom_signals.h"
+#include "constants.h"
+#include <unistd.h>
+#include <signal.h>
+#include "job.h"
+
+extern int foreground_pid;
+
+void handle_sigint(int signal)
+{
+    write(STDOUT_FILENO, "\nNice try, But you can't exit that easily!\n", 44);
+}
+
+/******** 
+*   NOTE: We were trying to create a log file with the history of the shell life 
+*    with time stamps but didnt have enough time to finish the implementation 
+*
+****/
+void handle_sigquit(int signal)
+{
+
+    write(STDOUT_FILENO, "Force quit was intiaited\n",26);
+    /* write(STDOUT_FILENO, "Creating a log file for saved history...\n",42); */
+
+}
+
+void handle_sigtstp(int singal)
+{
+
+    write(STDOUT_FILENO,"Control-Z was pressed\n",23);
+
+    // if (foreground_pid > 0)
+    // {   
+    //     write(1,"inside of foreg", 16);
+
+    //     kill(foreground_pid, SIGTSTP);  //send a signal 
+    // }
+
+
+}
